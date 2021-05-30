d6903a86faeefd91500b0be3b2e11bd2cf02ed45
Devid2005 <dh038550@gmail.com>
2021-05-30 16:33:27 -0500

    Proyecto Final

--- Final/servicioFinal.js
+++ Final/servicioFinal.js
@@ -1,9 +1,12 @@
+// https://pley.today/__export/1596596781723/sites/mui/img/2020/08/04/9cd077911a00165865a90ca5bf3d7fbd.jpg_697727896.jpg
+// https://static3.depositphotos.com/1008525/244/i/950/depositphotos_2444604-stock-photo-very-sad-little-boy.jpg
+// https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg
+
 const axios = require( 'axios' );
 
 const prompt = require( 'prompt-sync' )();
 
-var Rurl = prompt("--> ")
-
+var Rurl = prompt("Ingresa el url de la foto --> ")
 
 function primer(callback){
     var informacion;
@@ -11,13 +14,6 @@ function primer(callback){
     .then( respuesta =>{
         informacion = respuesta.data
         callback(informacion)
-    /*
-    for(i=0; i < informacion.length; i++){
-        var valor = informacion[i].sentimient;
-        var valor1 = informacion[i].url;
-        callback(valor,valor1)
-    }
-    */
     }).catch( error => {
     console.log( error );
     });
@@ -30,40 +26,16 @@ function segundo(llegada){
     var datosM = [];
     function tercero(callback){
         do{
-            //var positivo = (datos[i].sentimient == 'negative')
-             
-            //console.log( positivo.length )
-            //console.log( datos[i].id + ":" + positivo )
-          
             if(datos[i].sentimient == 'negative'){
                 datosN[i] = "Id: " + datos[i].id + ' || Nombre: ' + datos[i].nombre + ' || Autor: ' + datos[i].autor + ' || Url: ' + datos[i].url
-                //console.log(datosN[i])
-                //var datosN3 = "Negativos: " + datosN[i]
-                //console.log(  datosN3 )
-            
             } else{ datosN[i] = null }
             if(datos[i].sentimient == 'positive'){
                 datosP[i] = "Id: " + datos[i].id + ' || Nombre: ' + datos[i].nombre + ' || Autor: ' + datos[i].autor + ' || Url: ' + datos[i].url
-                //var datosP3 = "Positivos: " + datosP[i]
-                //console.log(  datosP3 )
-                //console.log(datosP[i])
-                //console.log(  "Positivos: " + datosP[i])
-                
-                //console.log( datos[i].id + ":" + datos1 )
-                
             } else{ datosP[i] = null }
             if(datos[i].sentimient == 'mixed'){
                 datosM[i] = "Id: " + datos[i].id + ' || Nombre: ' + datos[i].nombre + ' || Autor: ' + datos[i].autor + ' || Url: ' + datos[i].url
-                //console.log(datosM[i])
-                
-                //console.log(  "Neutrales: " + datosM[i])
-                
-                //console.log( datos[i].id + ":" + datos1 )
-                
             } else{ datosM[i] = null  }
-            
             i++
-        
         }while( i < datos.length )
         callback(datosN,datosP,datosM)
     }
@@ -71,19 +43,6 @@ function segundo(llegada){
         var valor2 = a1
         var valor3 = a2
         var valor4 = a3
-        //console.log(valor2[i])
-        //for(i=0; i<valor2.length; i++){
-
-          //  console.log(valor2[i]=="true")
-        //}
-        //console.log("afds: "+valor3)
-        //for(i=0;i<valor2.length;i++){
-          // data1 = valor2[i]
-        //}
-        //console.log(data1)
-
-
-        
         var datosImagen = { url: Rurl };
         var direccionImagen = 'https://servicioface.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=false&returnFaceAttributes=emotion';
         axios.post( direccionImagen, datosImagen, {
@@ -97,7 +56,6 @@ function segundo(llegada){
             var newDataNe = datosIma.anger + datosIma.contempt + datosIma.disgust + datosIma.fear + datosIma.sadness
             var newDataM = datosIma.neutral
             var newDataP = datosIma.happiness + datosIma.surprise
-            
                 console.log( " De acuerdo al analisis de su foto, sus sentimientos son(1 mayor nota): " )
                 if( datosIma.anger > 0 ){
                     console.log( " Furia: "+datosIma.anger )
@@ -142,22 +100,14 @@ function segundo(llegada){
                     }
                 }
             }
-
-
             var Rurl1 = prompt("Quiere traducir la letra de alguna cancion recomenadada(Yes o No): " );
             if( Rurl1 == 'Yes' ){
                 var arreglo = llegada;
-                //console.log("Coloca el id de la cancion: ")
-                //for( i =0; i < arreglo.length; i++){
-                    //var letra = arreglo[i].text;
                     var Rurl2 = prompt( "Coloca el id de la cancion: " );
                     var datosTexto = Number(Rurl2) - 1
-                    //console.log( arreglo[datosTexto].text )
                     var datosTexto1 = arreglo[datosTexto].text;
                     var idiomaPedido = prompt( "Coloca el id del idioma: Japones(ja) : Inglés(en) : Francés(fr) : Español(es) : Coreano(ko) :  Italiano(it) : Ruso(ru) --> " );
                     var datosTraducion = [{ "text" : datosTexto1 }]
-
-                    //console.log("Coloca el id del idioma: Japones(ja) : Inglés(en) : Francés(fr)")
                     var direccionTraduccion = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to="+idiomaPedido;
                     axios.post( direccionTraduccion, datosTraducion, {
                         headers:{
@@ -170,23 +120,11 @@ function segundo(llegada){
                     }).catch( error => {
                         console.log( error );
                     })
-                //}
             }
-            //console.log( datosIma )
         }).catch( error => {
             console.log( error );
         })
-        
-
     }
     tercero(cuatro)
 }
 primer(segundo)
-
-
-
-
-// https://marcianosmx.com/wp-content/uploads/2018/02/hombre-sonriendo-en-medio-de-una-carretera.jpg
-// https://pley.today/__export/1596596781723/sites/mui/img/2020/08/04/9cd077911a00165865a90ca5bf3d7fbd.jpg_697727896.jpg
-// https://static3.depositphotos.com/1008525/244/i/950/depositphotos_2444604-stock-photo-very-sad-little-boy.jpg
-// https://laboratoriosniam.com/wp-content/uploads/2018/07/michael-dam-258165-unsplash_WEB2.jpg
